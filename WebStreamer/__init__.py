f9d4cca7cd6b5c0e6ed9fc78b0f0a114b6dd5a99
streaam-developer <163761596+streaam-developer@users.noreply.github.com>
2024-07-13 18:37:51 +0530

    Update __init__.py

--- WebStreamer/__init__.py
+++ WebStreamer/__init__.py
@@ -3,5 +3,12 @@
 
 import time
 
+getLogger("aiohttp").setLevel(ERROR)
+getLogger("pyrogram").setLevel(ERROR)
+getLogger("aiohttp.web").setLevel(ERROR)
+
+LOGGER = getLogger(__name__)
+
+
 __version__ = "2.5.0"
 StartTime = time.time()
